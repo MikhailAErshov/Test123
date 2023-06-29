0906a628ef1c887971fae78a6745f61f640ed0fe
MikhailAErshov <aem.cmd@gmail.com>
2023-06-29 22:03:15 +0300

    asssfaa

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class MainTest {
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
+
+public class MainTest extends WebTest {
+    @Test
+    public void testLogin() {
+        open("https://www.google.ru");
+
+        $x("//textarea[@name='q']").click();
+        $x("//textarea[@name='q']").sendKeys("book");
+        $x("//textarea[@name='q']").pressEnter();
+    }
 }

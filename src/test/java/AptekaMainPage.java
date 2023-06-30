c3dd744e48aca2ae179ab41f3205b682545f4b63
MikhailAErshov <aem.cmd@gmail.com>
2023-06-30 13:14:28 +0300

    add tabcatalogue

--- src/test/java/AptekaMainPage.java
+++ src/test/java/AptekaMainPage.java
@@ -4,7 +4,7 @@ import static com.codeborne.selenide.Selenide.$x;
 
 public class AptekaMainPage {
     public SelenideElement confirmRegion = $x("//div[@class='confirm_region']");
-    public SelenideElement tabCatalogue = $x("//*[@id='app']/div[1]/header/div/div/div[1]/div/button");
+    public SelenideElement tabCatalogue = $x("//input[@id='title-search-input_fixed']");
     public SelenideElement tabCataloguePoint = $x("//span[contains(text(),'Гели для душа')]");
     public SelenideElement searchField = $x("//*[@id='title-search-input_fixed']");
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,11 +1,19 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static org.openqa.selenium.logging.LogType.DRIVER;
 
 public class MainTest extends WebTest {
 
@@ -13,13 +21,14 @@ public class MainTest extends WebTest {
     AptekaMainPage aptekaMainPage = new AptekaMainPage();
     @Test
     public void testLogin() {
-        open("https://habr.com/ru/all");
-//        Selenide.webdriver().driver().getWebDriver().manage().addCookie(new Cookie("current_region", "103006"));
-//        refresh();
+        open("https://aptekaeconom.com");
+        Selenide.webdriver().driver().getWebDriver().manage().addCookie(new Cookie("current_region", "103006"));
+        refresh();
+        new WebDriverWait(Selenide.webdriver().driver().getWebDriver(),Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='title-search-input_fixed']")));
 
-        sleep(10000);
-        aptekaMainPage.tabCatalogue.hover();
         aptekaMainPage.tabCatalogue.click();
+
 
 
 

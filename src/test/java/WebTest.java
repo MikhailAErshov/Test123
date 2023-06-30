95d9abedb759814d304fa11f463fe4e1a942b27b
MikhailAErshov <aem.cmd@gmail.com>
2023-06-30 15:28:15 +0300

    add tabcatalogue

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -5,25 +5,28 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
-import static com.codeborne.selenide.WebDriverRunner.setWebDriver;
 public class WebTest {
     @BeforeAll
     public static void setDriver() throws MalformedURLException {
-        boolean isRemote = true;
-        if (isRemote) {
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setBrowserName("chrome");
-            capabilities.setCapability("enableVNC:", true);
-            WebDriver driver = new RemoteWebDriver(URI.create("http://localhost:4444/wd/hub").toURL(), capabilities);
-            setWebDriver(driver);
-        } else {
-            Configuration.browser = "firefox";
-        }
+        Configuration.remote = "http://localhost:4444/wd/hub";
+        Configuration.reportsFolder = "target/surefire-reports";
+
+        Map<String, Boolean> options = new HashMap<>();
+        options.put("enableVNC", true);
+        options.put("enableLog", true);
+
+        ChromeOptions capabilities = new ChromeOptions();
+        capabilities.setBrowserVersion("114.0");
+        Configuration.browserCapabilities = capabilities;
+        Configuration.browserCapabilities.setCapability("selenoid:options", options);
     }
 }
